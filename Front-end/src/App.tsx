836a9af72db88d3e1ba4012c7bf13774d4dacf0d
scho-413 <jdfm0000@gmail.com>
2022-08-07 19:25:08 +1200

    Mock up design complete

--- Front-end/src/App.tsx
+++ Front-end/src/App.tsx
@@ -1,37 +1,83 @@
+import { height } from "@mui/system";
 import axios from "axios";
 import { useState } from 'react';
 import './App.css';
-import IconButton from "@mui/material/IconButton";
-import SearchIcon from "@mui/icons-material/Search";
-import TextField from "@mui/material/TextField";
 
 function App() {
   // Declare a new state variable, which we'll call "pokemonName"
   const [cocktailName, setCocktailName] = useState("");
+  const IngredientImage = "https://www.thecocktaildb.com/images/ingredients/";
   const [cocktailInfo, setCocktailInfo] = useState<undefined | any>(undefined);
 
   const COCKTAIL_BASE_URL = "https://www.thecocktaildb.com/api/json/v1/1/";
   return (
-    <div>
+    <div style={{ textAlign: "center" }}>
       <h1>
         Cocktail Ingredient Search
       </h1>
       
       <div>
         <label>Cocktail Name</label><br/>
-        <input type="text" id="cocktail-name" name="cocktail-name" onChange={e => setCocktailName(e.target.value)}/><br/>
+        <input type="text" id="cocktail-name" name="cocktail-name" onChange={e => setCocktailName(e.target.value)}/>
         <button onClick={search}>
         Search
         </button>
       </div>
 
-      <p>You have entered {cocktailName}</p>
-
       {cocktailInfo === undefined ? (
         <p>No recipe not found</p>
       ) : (
         <div id="cocktail-result">
-          <img src={cocktailInfo.drinks[0].strDrinkThumb} />
+          <table style={{ width: '100%' }}>
+            <tbody>
+              <tr>
+                <td style={{ width: "35%" }}>
+                  <h2>{cocktailInfo.drinks[0].strDrink}</h2>
+                </td>
+                <td style={{ width: "60%" }}>
+                  <h2>Ingredients</h2>
+                </td>
+              </tr>
+
+              <td style={{ width: "35%" }}>
+                <img src={cocktailInfo.drinks[0].strDrinkThumb} style={{ width: "80%", height: "80%" }}></img>
+              </td>
+              <td style={{ width: "60%" }}>
+                <table style={{ width: "100%" }}>
+                  <tr>
+                    <td>
+                      <img src={IngredientImage + cocktailInfo.drinks[0].strIngredient1 + '.png'} style={{ width: "50%", height: "50%" }}></img>
+                      <figcaption>{cocktailInfo.drinks[0].strMeasure1} {cocktailInfo.drinks[0].strIngredient1}</figcaption>
+                    </td>
+                    <td>
+                      <img src={IngredientImage + cocktailInfo.drinks[0].strIngredient2 + '.png'} style={{ width: "50%", height: "50%" }}></img>
+                      <figcaption>{cocktailInfo.drinks[0].strMeasure2} {cocktailInfo.drinks[0].strIngredient2}</figcaption></td>
+                    <td>
+                      <img src={IngredientImage + cocktailInfo.drinks[0].strIngredient3 + '.png'} style={{ width: "50%", height: "50%" }}></img>
+                      <figcaption>{cocktailInfo.drinks[0].strMeasure3} {cocktailInfo.drinks[0].strIngredient3}</figcaption>
+                    </td>
+                  </tr>
+                  <tr>
+                  <td>
+                      <img src={IngredientImage + cocktailInfo.drinks[0].strIngredient4 + '.png'} style={{ width: "50%", height: "50%" }}></img>
+                      <figcaption>{cocktailInfo.drinks[0].strMeasure4} {cocktailInfo.drinks[0].strIngredient4}</figcaption>
+                    </td>
+                    <td>
+                      <img src={IngredientImage + cocktailInfo.drinks[0].strIngredient5 + '.png'} style={{ width: "50%", height: "50%" }}></img>
+                      <figcaption>{cocktailInfo.drinks[0].strMeasure5} {cocktailInfo.drinks[0].strIngredient5}</figcaption></td>
+                  </tr>
+                  <tr>
+
+                  </tr>
+                </table>
+              </td>
+            </tbody>
+          </table>
+
+          <h2>Instructions</h2>
+          <div>
+            { cocktailInfo.drinks[0].strInstructions }
+          </div>
         </div>
       )}
     </div>
@@ -43,6 +89,7 @@ function App() {
       console.log(res.data.drinks[0]);
     });
   }
+
 }
 
 export default App;
