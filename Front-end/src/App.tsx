1b76edd7fa7a00194a70de40f49bb7e7d96e76cb
scho-413 <jdfm0000@gmail.com>
2022-08-07 18:31:11 +1200

    basic functionality made

--- Front-end/src/App.tsx
+++ Front-end/src/App.tsx
@@ -1,173 +1,50 @@
 import axios from "axios";
-import { Pokemon } from "pokenode-ts";
-import { useState } from "react";
+import { useState } from 'react';
+import './App.css';
+import IconButton from "@mui/material/IconButton";
 import SearchIcon from "@mui/icons-material/Search";
 import TextField from "@mui/material/TextField";
-import "./App.css";
-import { Box, Button, Grid, Paper, Skeleton } from "@mui/material";
 
 function App() {
-  const [pokemonName, setPokemonName] = useState("");
-  const [pokemonInfo, setPokemonInfo] = useState<null | undefined | Pokemon>(
-    undefined
-  );
-  const POKEMON_BASE_API_URL = "https://pokeapi.co/api/v2";
+  // Declare a new state variable, which we'll call "pokemonName"
+  const [cocktailName, setCocktailName] = useState("");
+  const [cocktailInfo, setCocktailInfo] = useState<undefined | any>(undefined);
+
+  const COCKTAIL_BASE_URL = "https://www.thecocktaildb.com/api/json/v1/1/";
   return (
     <div>
-      <div className="search-field">
-        <h1>Pokédex Search</h1>
-        <div style={{ display: "flex", justifyContent: "center" }}>
-          <TextField
-            id="search-bar"
-            className="text"
-            value={pokemonName}
-            onChange={(prop) => {
-              setPokemonName(prop.target.value);
-            }}
-            label="Enter a Pokémon Name..."
-            variant="outlined"
-            placeholder="Search..."
-            size="medium"
-          />
-          <Button
-            onClick={() => {
-              search();
-            }}
-          >
-            <SearchIcon style={{ fill: "blue" }} />
-            Search
-          </Button>
-        </div>
+      <h1>
+        Cocktail Ingredient Search
+      </h1>
+      
+      <div>
+        <label>Cocktail Name</label><br/>
+        <input type="text" id="cocktail-name" name="cocktail-name" onChange={e => setCocktailName(e.target.value)}/><br/>
+        <button onClick={search}>
+        Search
+        </button>
       </div>
 
-      {pokemonInfo === undefined ? (
-        <div></div>
+      <p>You have entered {cocktailName}</p>
+
+      {cocktailInfo === undefined ? (
+        <p>No recipe not found</p>
       ) : (
-        <div
-          id="pokemon-result"
-          style={{
-            maxWidth: "800px",
-            margin: "0 auto",
-            padding: "100px 10px 0px 10px",
-          }}
-        >
-          <Paper sx={{ backgroundColor: getBackColor(pokemonInfo) }}>
-            <Grid
-              container
-              direction="row"
-              spacing={5}
-              sx={{
-                justifyContent: "center",
-              }}
-            >
-              <Grid item>
-                <Box>
-                  {pokemonInfo === undefined || pokemonInfo === null ? (
-                    <h1> Pokemon not found</h1>
-                  ) : (
-                    <div>
-                      <h1>
-                        {pokemonInfo.name.charAt(0).toUpperCase() +
-                          pokemonInfo.name.slice(1)}
-                      </h1>
-                      <p>
-                        ID: {pokemonInfo.id}
-                        <br />
-                        Height: {pokemonInfo.height * 10} cm
-                        <br />
-                        Weight: {pokemonInfo.weight / 10} kg
-                        <br />
-                        Types: {getTypes()?.toString()}
-                        <br />
-                        Abilities: {getAbilities()?.toString()}
-                      </p>
-                    </div>
-                  )}
-                </Box>
-              </Grid>
-              <Grid item>
-                <Box>
-                  {pokemonInfo?.sprites.other.dream_world.front_default ? (
-                    <img
-                      height="300px"
-                      width="300px"
-                      alt={pokemonInfo.name}
-                      src={pokemonInfo.sprites.other.dream_world.front_default}
-                    ></img>
-                  ) : (
-                    <Skeleton width={300} height={300} />
-                  )}
-                </Box>
-              </Grid>
-            </Grid>
-          </Paper>
+        <div id="cocktail-result">
+          <img src={cocktailInfo.drinks[0].strDrinkThumb} />
         </div>
       )}
     </div>
   );
 
-  // Credit to codingsparkles for providing the color mapping
-  function getBackColor(poke: Pokemon | undefined | null) {
-    let backColor = "#EEE8AA";
-    if (poke === undefined || poke === null) {
-      return backColor;
-    }
-    const pokeTypes = poke.types.map((i) => i.type.name);
-    if (pokeTypes.includes("fire")) {
-      backColor = "#FEC5BB";
-    } else if (pokeTypes.includes("grass")) {
-      backColor = "#80FFDB";
-    } else if (pokeTypes.includes("water")) {
-      backColor = "#DFE7FD";
-    } else if (pokeTypes.includes("bug")) {
-      backColor = "#B0DEA3";
-    } else if (pokeTypes.includes("normal")) {
-      backColor = "#E0FFFF";
-    } else if (pokeTypes.includes("electric")) {
-      backColor = "#D8E2DC";
-    } else if (pokeTypes.includes("ground")) {
-      backColor = "#FAD2E1";
-    } else if (pokeTypes.includes("fairy")) {
-      backColor = "#FFF1E6";
-    } else if (pokeTypes.includes("ghost")) {
-      backColor = "#F8EDEB";
-    } else if (pokeTypes.includes("fighting")) {
-      backColor = "#F1FAEE";
-    } else if (pokeTypes.includes("rock")) {
-      backColor = "#A8DADC";
-    }
-    return backColor;
-  }
-
   function search() {
-    console.log(pokemonName);
-    if (pokemonName === undefined || pokemonName === "") {
-      return;
-    }
-
-    axios
-      .get(POKEMON_BASE_API_URL + "/pokemon/" + pokemonName?.toLowerCase())
-      .then((res) => {
-        setPokemonInfo(res.data);
-      })
-      .catch(() => {
-        setPokemonInfo(null);
-      });
-  }
-
-  function getTypes() {
-    if (pokemonInfo !== undefined && pokemonInfo !== null) {
-      return pokemonInfo.types.map((item) => item.type.name);
-    }
-  }
-
-  function getAbilities() {
-    if (pokemonInfo !== undefined && pokemonInfo !== null) {
-      return pokemonInfo.abilities.map((ability) => ability.ability.name);
-    }
+    axios.get(COCKTAIL_BASE_URL + "search.php?s=" + cocktailName).then((res) => {
+      setCocktailInfo(res.data);
+      console.log(res.data.drinks[0]);
+    });
   }
 }
 
 export default App;
 
-//https://mixedanalytics.com/blog/list-actually-free-open-no-auth-needed-apis/
+//https://www.thecocktaildb.com/api.php
